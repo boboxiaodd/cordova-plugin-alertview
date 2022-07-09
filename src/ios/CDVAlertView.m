6c765d640d7864228b7d2fe67b39c19c91f81a0a
boboxiaodd <iam386@gmail.com>
2022-07-09 19:01:34 +0800

    add

---
+++ src/ios/CDVAlertView.m
@@ -0,0 +1,28 @@
+#import <Cordova/CDV.h>
+#import "CDVAlertView.h"
+
+
+@interface CDVAlertView ()
+
+@end
+
+@implementation CDVAlertView
+
+
+
+#pragma mark 公共函数
+
+- (void)send_event:(CDVInvokedUrlCommand *)command withMessage:(NSDictionary *)message Alive:(BOOL)alive State:(BOOL)state{
+    if(!command) return;
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus: (state ? CDVCommandStatus_OK : CDVCommandStatus_ERROR) messageAsDictionary:message];
+    if(alive) [res setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult: res callbackId: command.callbackId];
+}
+
+-(void)touchfeedback
+{
+    UIImpactFeedbackGenerator *feedBackGenertor = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
+    [feedBackGenertor impactOccurred];
+}
+
+@end
