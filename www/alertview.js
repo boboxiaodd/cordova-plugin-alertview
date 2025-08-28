f819137d00450d728d9281aa5c6852c94145afe8
boboxiaodd <iam386@gmail.com>
2025-08-28 14:48:34 +0800

    Update alertview.js
    remove scl

--- www/alertview.js
+++ www/alertview.js
@@ -3,14 +3,8 @@ const CDVAlertSheet = {
     alert:function (success,option){
         exec(success,null,'CDVAlertSheet','alert',[option]);
     },
-    alert_scl:function (success,option){
-        exec(success,null,'CDVAlertSheet','alert_scl',[option]);
-    },
     confirm:function (success,option) {
         exec(success,null,'CDVAlertSheet','confirm',[option]);
-    },
-    confirm_scl:function (success,option) {
-        exec(success,null,'CDVAlertSheet','confirm_scl',[option]);
     },
     showLoadding:function (option) {
         exec(null,null,'CDVAlertSheet','showLoadding',[option]);
